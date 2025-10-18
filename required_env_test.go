7e58d2c8eb151c30d6bfef507523560c644b9c42
soumayg9673 <pmrs9673@gmail.com>
2025-10-19 04:47:37 +0530

    unit tests for AddMulRqdKeys func

--- required_env_test.go
+++ required_env_test.go
@@ -60,3 +60,72 @@ func TestAddRqdKey(t *testing.T) {
 		})
 	}
 }
+
+func TestAddMulRqdKeys(t *testing.T) {
+	testCases := []struct {
+		Name   string
+		MapRqd map[string]bool
+		Err    error
+	}{
+		{
+			Name: "map of keys with value required",
+			MapRqd: map[string]bool{
+				"PACKAGE": true,
+				"PUBLIC":  true,
+			},
+			Err: nil,
+		},
+		{
+			Name: "map of keys with no value required",
+			MapRqd: map[string]bool{
+				"PACKAGE": false,
+				"PUBLIC":  false,
+			},
+			Err: nil,
+		},
+		{
+			Name: "map of empty key with value required",
+			MapRqd: map[string]bool{
+				"": true,
+			},
+			Err: dotenv.ErrEmptyKey,
+		},
+		{
+			Name: "map of empty key with no value required",
+			MapRqd: map[string]bool{
+				"": false,
+			},
+			Err: dotenv.ErrEmptyKey,
+		},
+		{
+			Name: "map of empty/on-empty keys with value, no-value required",
+			MapRqd: map[string]bool{
+				"":        false,
+				"PACKAGE": true,
+			},
+			Err: dotenv.ErrEmptyKey,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			if err := dotenv.AddMulRqdKeys(tc.MapRqd); err != nil {
+				if !errors.Is(tc.Err, err) { // failed
+					t.Errorf("expected error %v but got %v", tc.Err, err)
+				} else { // pass
+					return
+				}
+			}
+
+			m := dotenv.GetAllRqd()
+
+			for k := range tc.MapRqd {
+				if _, ok := m[k]; !ok {
+					t.Error("key should be listed on required list")
+				}
+				// clean up required env keys
+				dotenv.DeleteRqdKey(k)
+			}
+		})
+	}
+}
