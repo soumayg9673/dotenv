700e9d6576f20a345a448afbe6e3bc7c3f705ddb
soumayg9673 <pmrs9673@gmail.com>
2025-10-18 07:15:37 +0530

    add example how to use package

---
+++ example/example.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/soumayg9673/dotenv"
+)
+
+func main() {
+	// Setting required key
+	dotenv.AddRqdKey("AUTHOR", true)
+
+	if err := dotenv.LoadEnvFile("./example/test.env"); err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(os.Getenv("AUTHOR"))
+
+	if err := dotenv.ValidateRqdEnv(); err != nil {
+		fmt.Println(err)
+	}
+}
